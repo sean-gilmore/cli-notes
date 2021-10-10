e8ef2aecf7785cc76f5bbf00c3f9bbef300dd055
Sean <sean@arcadiandigital.com.au>
2021-10-10 20:47:01 +1100

    Fix: Correct tests

--- test/services/Collection.test.ts
+++ test/services/Collection.test.ts
@@ -9,16 +9,16 @@ describe("Collection", () => {
   test.it("returns the test directory structure", () => {
     const collectionTree = Collection.getTree();
     const testTree = {
-      name: './test-dir',
+      path: './test-dir',
       projects: [
-        { name: './test-dir/config', projects: [] },
-        { name: './test-dir/project1', projects: [] },
-        { name: './test-dir/project2', projects: [] },
-        { name: './test-dir/project3', projects: [] }
+        { path: './test-dir/config', projects: [] },
+        { path: './test-dir/project1', projects: [] },
+        { path: './test-dir/project2', projects: [] },
+        { path: './test-dir/project3', projects: [] }
       ]
     }
 
-    expect(collectionTree.name).to.eq(testTree.name);
-    expect(collectionTree.projects[0].name).to.eq(testTree.projects[0].name);
+    expect(collectionTree.path).to.eq(testTree.path);
+    expect(collectionTree.projects[0].path).to.eq(testTree.projects[0].path);
   });
 });
